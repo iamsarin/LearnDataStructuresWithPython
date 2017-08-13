2f7b4240668a07dc41cc2105e1c291a863886b3b
SarinPC <iamsarin94@gmail.com>
2017-08-13 15:02:04 +0700

    - fix delete node in singly linked list - add doubly linked list

--- L03-DoublyLinkedList/index.py
+++ L03-DoublyLinkedList/index.py
@@ -1,20 +1,20 @@
-class Node:
+class DoublyNode:
     info = None
     nextNode = None
+    previousNode = None
 
 
-class LinkedList:
+class DoublyLinkedList:
     count: int = 0
-    current: Node = None
-    first: Node = None
-    last: Node = None
+    current: DoublyNode = None
+    first: DoublyNode = None
+    last: DoublyNode = None
 
     def __init__(self, node=None):
         self.count = 0
         self.current = None
         self.first = None
         self.last = None
-
         if node is not None:
             self.copy(node)
 
@@ -30,9 +30,22 @@ class LinkedList:
 
         self.current = self.first
 
-        while self.current:
+        while self.current is not None:
             print(self.current.info, ' ', end='')
             self.current = self.current.nextNode
+
+        self.current = None
+        print()
+
+    def print_reverse(self):
+        if self.count == 0:
+            return
+
+        self.current = self.last
+
+        while self.current is not None:
+            print(self.current.info, ' ', end='')
+            self.current = self.current.previousNode
 
         self.current = None
         print()
@@ -77,10 +90,11 @@ class LinkedList:
         return False
 
     def insert_first(self, info):
-        node_to_insert_at_the_first = Node()
+        node_to_insert_at_the_first = DoublyNode()
         node_to_insert_at_the_first.info = info
 
         node_to_insert_at_the_first.nextNode = self.first
+        self.first.previousNode = node_to_insert_at_the_first
 
         self.first = node_to_insert_at_the_first
         self.count = self.count + 1
@@ -89,7 +103,7 @@ class LinkedList:
             self.last = node_to_insert_at_the_first
 
     def insert_last(self, info):
-        node_to_insert_at_the_last = Node()
+        node_to_insert_at_the_last = DoublyNode()
         node_to_insert_at_the_last.info = info
 
         self.count = self.count + 1
@@ -99,23 +113,26 @@ class LinkedList:
             self.last = node_to_insert_at_the_last
         else:
             self.last.nextNode = node_to_insert_at_the_last
+            node_to_insert_at_the_last.previousNode = self.last
             self.last = node_to_insert_at_the_last
 
     def delete_node(self, info):
-        node_before_running = None
         node_running = self.first
 
         while node_running is not None:
             if info == node_running.info:
                 self.count = self.count - 1
-                if node_before_running is None:
+                if node_running.previousNode is None:
+                    node_running.previousNode = None
                     self.first = node_running.nextNode
+
                 else:
-                    node_before_running.nextNode = node_running.nextNode
+                    node_running.previousNode.nextNode = node_running.nextNode
+
                     if node_running.nextNode is None:
-                        self.last = node_before_running
-            else:
-                node_before_running = node_running
+                        self.last = node_running.previousNode
+                    else:
+                        node_running.nextNode.previousNode = node_running.previousNode
 
             node_running = node_running.nextNode
 
@@ -135,9 +152,9 @@ class LinkedList:
             self.current = self.current.nextNode
 
 
-a = LinkedList()
+a = DoublyLinkedList()
 print('Hi This is my linked list.')
-print('_______________________________________________________')
+print('________________________1______________________________')
 print('I will insert 309, 30, 302, 302, 304')
 a.insert_last(309)
 a.insert_last(30)
@@ -146,27 +163,34 @@ a.insert_last(302)
 a.insert_last(304)
 print('After insert my linked list is')
 a.print()
-print('_______________________________________________________')
+print('________________________2______________________________')
+print('My linked list show in reverse is')
+a.print_reverse()
+print('________________________3______________________________')
 print('I don\'t like 30 I will delete 30')
+a.delete_node(30)
 print('After I delete 30 my linked list is')
 a.print()
-print('_______________________________________________________')
-print('I don\'t like 30 I will delete 304')
+print('________________________4______________________________')
+print('My linked list show in reverse is')
+a.print_reverse()
+print('________________________5______________________________')
+print('I don\'t like 304 I will delete 304')
 a.delete_node(304)
 print('After I delete 304 my last item is')
 print(a.back())
-print('_______________________________________________________')
+print('________________________6______________________________')
 print('Now I want 30 back, but I want two in fist and last')
 a.insert_first(30)
 a.insert_last(30)
 print('After I do my linked list is')
 a.print()
-print('_______________________________________________________')
+print('________________________7______________________________')
 print('I don\'t like 302 I will delete 302')
 a.delete_node(302)
 print('After I delete 302 my linked list is')
 a.print()
-print('_______________________________________________________')
+print('________________________8______________________________')
 print('Now my linked list has many item, I cannot remember that has 90 item')
 a.delete_node(302)
 print('Ok! my linked list can search....')
@@ -174,17 +198,18 @@ if a.search(90):
     print('Result: my linked list has 90')
 else:
     print('Result: my linked list does not has 90')
-print('_______________________________________________________')
+print('_________________________9_____________________________')
 print('I want to copy my linked list to another that name AAA')
-AAA = LinkedList(a)
+AAA = DoublyLinkedList(a)
 print('Now copy successful my AAA is')
 AAA.print()
 print('I want to add 90 to AAA, my AAA is')
 AAA.insert_last(90)
 AAA.print()
-print('_______________________________________________________')
+print('_________________________10____________________________')
 print('I will show my 2 list')
 print('My Linked List: ')
 a.print()
 print('AAA: ')
 AAA.print()
+
